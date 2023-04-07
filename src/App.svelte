81382197eb636129fe4de5c9f96173278e144b22
Juan Israel dos Anjos <86119976+JuanIWK3@users.noreply.github.com>
2023-04-07 13:13:07 -0300

    feat: backtracking

--- src/App.svelte
+++ src/App.svelte
@@ -1,120 +1,172 @@
 <script lang="ts">
-  let size = 6;
+  let speed = 1;
+  let size = 10;
   let pos = { x: 0, y: 0 };
   let visited = new Set();
   let prev = new Map();
   let visitedFrom = new Map();
   let stuck = 0;
+  let prevDirection = 0;
 
-  let getDirection = () => {
-    return Math.floor(Math.random() * 4);
+  let getRandomUniqueDirection = () => {
+    let direction = Math.floor(Math.random() * 4);
+
+    if (direction === prevDirection) {
+      direction = (direction + 1) % 4;
+    }
+
+    prevDirection = direction;
+
+    return direction;
   };
 
-  let start = setInterval(() => {
-    move();
-    // console.log(visitedFrom);
-
-    if (visited.size === size * size) {
-      console.log("done");
-      clearInterval(start);
-    }
-
-    if (stuck > 50) {
-      console.log("stuck");
-      clearInterval(start);
-    }
-  }, 10);
-
   let move = () => {
-    let direction = getDirection();
+    const direction = getRandomUniqueDirection();
     let next = { x: pos.x, y: pos.y };
 
-    switch (direction) {
-      case 0:
-        next.x++;
-        break;
-      case 1:
-        next.x--;
-        break;
-      case 2:
-        next.y++;
-        break;
-      case 3:
-        next.y--;
-        break;
-    }
+    const moveUp = () => {
+      prev.set(`${next.x}:${next.y}`, "2");
+    };
 
-    if (next.x < 0 || next.x >= size || next.y < 0 || next.y >= size) {
-      return;
+    const moveDown = () => {
+      prev.set(`${next.x}:${next.y}`, "0");
+    };
+
+    const moveLeft = () => {
+      prev.set(`${next.x}:${next.y}`, "1");
+    };
+
+    const moveRight = () => {
+      prev.set(`${next.x}:${next.y}`, "3");
+    };
+
+    if (
+      direction === 0 &&
+      pos.x !== 0 &&
+      !visitedFrom.has(`${pos.x - 1}:${pos.y}`)
+    ) {
+      next.x--;
+      moveUp();
+    } else if (
+      direction === 1 &&
+      pos.y !== 0 &&
+      !visitedFrom.has(`${pos.x}:${pos.y - 1}`)
+    ) {
+      next.y--;
+      moveLeft();
+    } else if (
+      direction === 2 &&
+      pos.x !== size - 1 &&
+      !visitedFrom.has(`${pos.x + 1}:${pos.y}`)
+    ) {
+      next.x++;
+      moveDown();
+    } else if (
+      direction === 3 &&
+      pos.y !== size - 1 &&
+      !visitedFrom.has(`${pos.x}:${pos.y + 1}`)
+    ) {
+      next.y++;
+      moveRight();
     }
 
     if (visited.has(`${next.x}:${next.y}`)) {
       stuck++;
-      return;
+    } else {
+      visited.add(`${next.x}:${next.y}`);
+      visitedFrom.set(`${next.x}:${next.y}`, direction);
+      pos = next;
+      stuck = 0;
     }
+  };
 
-    visited.add(`${pos.x}:${pos.y}`);
+  let start = () => {
+    visited.clear();
+    prev.clear();
+    visitedFrom.clear();
+    stuck = 0;
+    pos = { x: 0, y: 0 };
+    visited.add("0:0");
 
-    visitedFrom.set(`${next.x}:${next.y}`, `${direction}`);
+    const int = setInterval(() => {
+      if (visited.size === size * size) {
+        console.log("done");
+        clearInterval(int);
+      }
 
-    prev.set(`${next.x}:${next.y}`, `${pos.x}:${pos.y}`);
+      move();
 
-    pos = next;
+      if (visited.size === size * size) {
+        console.log("done");
+        clearInterval(int);
+      }
+
+      if (stuck > 4) {
+        if (prev.size === 0) {
+          console.log("stuck");
+          clearInterval(int);
+        } else {
+          console.log("backtracking");
+
+          const last = Array.from(prev.keys()).pop();
+          const lastDirection = prev.get(last);
+          prev.delete(last);
+
+          const [x, y] = last.split(":");
+          pos = { x: +x, y: +y };
+
+          visitedFrom.set(last, lastDirection);
+          stuck = 0;
+        }
+      }
+    }, speed);
+
+    return () => {
+      clearInterval(int);
+    };
   };
 
   const getVisitedFrom = (x: number, y: number) => {
     return visitedFrom.get(`${x}:${y}`);
   };
+
+  const reset = () => {
+    visited.clear();
+    prev.clear();
+    visitedFrom.clear();
+    stuck = 0;
+    pos = { x: 0, y: 0 };
+  };
 </script>
 
 <main>
-  <label for="size">
-    <p>Size</p>
-    <input min="2" id="size" type="number" bind:value={size} />
-  </label>
+  <h1 class="title">Maze Generator</h1>
+
+  <div class="options">
+    <label for="size">
+      <p>Size</p>
+      <input
+        min="2"
+        id="size"
+        type="number"
+        bind:value={size}
+        on:change={reset}
+      />
+    </label>
+    <label for="speed">
+      <p>Delay</p>
+      <input min="1" id="speed" type="number" bind:value={speed} />
+    </label>
+    <button on:click={() => start()}>Gen</button>
+  </div>
 
   <div class="maze">
     {#each Array(size) as _, i}
       <div class="row">
         {#each Array(size) as _, j}
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
           <div
-            class="cell
-              {i === 0 ? 'border-left' : ''}
-              {i === size - 1 ? 'border-bottom' : ''}
-              {j === 0 ? 'border-left' : ''}
-              {j === size - 1 ? 'border-right' : ''}
-              {visited.has(`${i}:${j}`) ? 'visited' : ''}"
-            style={`
-              background-color: ${
-                i === pos.x && j === pos.y ? "rgb(88, 141, 255)" : ""
-              };
-              color: ${visited.has(`${i}:${j}`) ? "#666" : "#333"};
-              ${
-                getVisitedFrom(i, j) === "0"
-                  ? "border-right: 1px solid white;border-left: 1px solid white;"
-                  : ""
-              }
-              ${
-                getVisitedFrom(i, j) === "1"
-                  ? "border-top: 1px solid white;border-bottom: 1px solid white;"
-                  : ""
-              }
-              ${
-                getVisitedFrom(i, j) === "2"
-                  ? "border-right: 1px solid white;border-left: 1px solid white;"
-                  : ""
-              }
-              ${
-                getVisitedFrom(i, j) === "3"
-                  ? "border-top: 1px solid white;border-bottom: 1px solid white;"
-                  : ""
-              }
-             
-            `}
-            on:click={() => {
-              pos = { x: i, y: j };
-            }}
+            class="cell {visited.has(`${i}:${j}`) ? 'visited' : ''} 
+            {pos.x === i && pos.y === j ? 'current' : ''}"
           >
             {i}:{j}
           </div>
@@ -127,20 +179,30 @@
 <style lang="scss">
   $accent: rgb(88, 141, 255);
 
-  main {
+  .options {
+    padding: 1em;
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    align-items: end;
     justify-content: center;
+    gap: 1em;
+
+    input {
+      width: 6em;
+    }
   }
 
   .maze {
+    flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     border: 1px solid $accent;
     user-select: none;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    aspect-ratio: 1/1;
 
     .row {
       display: flex;
@@ -148,61 +210,29 @@
       align-items: center;
       justify-content: center;
 
+      width: 100%;
+      height: 100%;
+
       .cell {
-        width: 3rem;
-        height: 3rem;
         display: flex;
         align-items: center;
         justify-content: center;
         color: #444;
         transition: background-color 0.2s ease-in-out;
+        width: 100%;
+        height: 100%;
+        font-size: small;
 
-        &.from-0 {
-          border-right: 1px solid red;
-          border-left: 1px solid red;
-        }
-        &.from-1 {
-          border-top: 1px solid red;
-          border-bottom: 1px solid red;
-        }
-        &.from-2 {
-          border-right: 1px solid red;
-          border-left: 1px solid red;
-        }
-        &.from-3 {
-          border-top: 1px solid red;
-          border-bottom: 1px solid red;
+        &.visited {
+          color: $accent;
+          background: #333;
         }
 
-        &.border-left {
-          border-left: none !important;
-        }
-        &.border-right {
-          border-right: none !important;
-        }
-        &.border-top {
-          border-top: none !important;
-        }
-        &.border-bottom {
-          border-bottom: none !important;
+        &.current {
+          background-color: $accent;
+          color: #fff;
         }
       }
-    }
-  }
-
-  label {
-    display: flex;
-    align-items: center;
-    width: max-content;
-    margin-bottom: 2rem;
-    gap: 1rem;
-
-    input {
-      padding: 0.6rem;
-      border-radius: 5px;
-      border: none;
-
-      color: $accent;
     }
   }
 </style>
